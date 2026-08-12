7978564389f46d4857a8999bdbbc4dc427bdaccd
claudiamv31 <claudiavega3108@gmail.com>
2026-08-11 20:48:41 -0700

    implement syntomps

--- backend.Tests/obj/Debug/net8.0/backend.Tests.AssemblyInfo.cs
+++ backend.Tests/obj/Debug/net8.0/backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e3ff8267ec3fedfbf1ed30c5b20fdf39a929e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d70efdc3608454a197dd74d5865dd2b8af9eed4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
